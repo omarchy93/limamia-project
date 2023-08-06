b4b79f7eae78bbae44ed5e0f48922a67d30660e6
omar <omarfarukchy93@gmail.com>
2023-08-07 00:50:52 +0600

    add blog page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,94 @@
+import Image from "next/image";
 import React from "react";
 
-const BolgId = () => {
-  return <div>BolgId</div>;
+const BolgPost = () => {
+  return (
+    <div>
+      <div className="flex gap-10 p-5 m-5">
+        <div className="flex-1 flex flex-col justify-start gap-6 text-justify">
+          <h1 className="text-xl font-bold">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus, ad tempora?
+          </h1>
+          <p className="text-base font-semibold">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
+            vel labore facere, mollitia fugiat culpa vitae iure velit voluptates
+            magni eligendi aspernatur, molestias at officia deserunt nobis
+            explicabo quos quod Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Molestiae iusto vero aliquid nulla, porro quae
+            voluptatem enim, quasi excepturi aspernatur, quos ipsa! Beatae
+            placeat voluptatem quia culpa quo ipsum ea!
+          </p>
+        </div>
+        <div className=" flex-1 h-[400px] w-[420px] relative">
+          <Image
+            className=""
+            src={
+              "https://images.pexels.com/photos/1809644/pexels-photo-1809644.jpeg?auto=compress&cs=tinysrgb&w=600"
+            }
+            alt="hi"
+            fill={true}
+          />
+        </div>
+      </div>
+      <div className="p-5 m-5 ">
+        <div className="h-10 w-10 relative ">
+          <Image
+            src={
+              "https://images.pexels.com/photos/1809644/pexels-photo-1809644.jpeg?auto=compress&cs=tinysrgb&w=600"
+            }
+            alt="clint icon"
+            fill={true}
+            className="rounded-full"
+          />
+          <div className="m-14">
+            <p className="font-bold text-lg"> debaroti borsha</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-5 m-5 text-justify font-semibold">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          nostrum quibusdam laboriosam ea saepe animi labore facere reiciendis
+          tempora! Ipsam, cum. Enim numquam qui quasi voluptas distinctio
+          ratione debitis at?
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          doloremque minima incidunt tenetur voluptates laboriosam explicabo
+          exercitationem dignissimos deserunt totam autem dolore repellat omnis
+          ad mollitia error obcaecati quibusdam adipisci sint alias accusamus
+          porro, placeat similique facere. Minus a inventore cum maxime porro
+          corporis veniam aperiam. Sunt quibusdam similique cumque hic libero
+          eum aspernatur nihil saepe ratione necessitatibus rerum officia
+          suscipit quae dolor nemo aliquid accusantium, in, repellendus
+          cupiditate iure totam dicta consequatur, a optio. Accusantium nisi
+          magnam facilis natus similique error neque explicabo quo dicta
+          perspiciatis sapiente sequi excepturi, ex aspernatur repellendus
+          laborum veniam quasi tempora nam libero quaerat.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ipsum
+          sequi pariatur cum debitis ea eveniet, quis labore nobis commodi at
+          voluptatum in alias recusandae odio? Dignissimos ipsa temporibus
+          harum!
+          <br />
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
+          cumque quam aperiam alias delectus, amet aspernatur nisi harum libero,
+          iusto, facilis aliquam ad molestias quis officiis ab laborum ducimus.
+          Ea! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Asperiores blanditiis consequuntur sed accusamus quas dolor quibusdam
+          explicabo nemo qui non fugit nobis, commodi deserunt nisi dolores
+          culpa quam. Incidunt, quo. Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Necessitatibus assumenda quod at perferendis odio,
+          cupiditate et qui maxime quia laboriosam? Ipsum, cum quibusdam sed
+          magnam error corporis veritatis facilis praesentium!
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default BolgId;
+export default BolgPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,69 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: { revalidate: 3600 },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const imgData = [
+  {
+    imgurl:
+      "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&w=900",
+  },
+
+  {
+    imgurl:
+      "https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=900",
+  },
+
+  {
+    imgurl:
+      "https://images.pexels.com/photos/2440021/pexels-photo-2440021.jpeg?auto=compress&cs=tinysrgb&w=900",
+  },
+  {
+    imgurl:
+      "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+];
+
+// eslint-disable-next-line @next/next/no-async-client-component
+const Blog = async () => {
+  const data = await getData();
   return (
     <div>
-      <h1>Blog page</h1>
+      {data.map((item) => {
+        const imgRandomItem = Math.floor(Math.random() * imgData.length);
+
+        const imgItem = (imgRandomItem, imgData[imgRandomItem]);
+        return (
+          <Link href="/blog/textId" key={item.id}>
+            <div className="flex gap-5">
+              <div className=" flex-1 my-5 h-[300px] w-[400px] relative">
+                I
+                <Image
+                  src={imgItem.imgurl}
+                  alt="bolg img"
+                  fill={true}
+                  className="object-cover"
+                />
+              </div>
+              <div className="flex-1 flex flex-col  justify-center gap-5">
+                <h2 className="text-2xl font-bold ">{item.title}</h2>
+                <p className=" text-lg font-semibold">{item.body}</p>
+              </div>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
